8b1e0f9175b7f1e09a188477307e90ee107576b6
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-28 11:21:31 -0700

    setup: init-prettier with Tailwind CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +14,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
+    rules: {
+      "prettier/prettier": "error"
+    }
+  }
 ];
 
 export default eslintConfig;
