31974d801d0cc5f37884bfcf182a41eee491f393
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-29 11:36:11 -0700

    feat: Adjust padding and layout for responsive design

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className='p-4'>
-      <div className='mx-auto flex w-full max-w-80 items-baseline justify-between'>
+    <header className='px-4 pt-4 pb-5 sm:px-8 sm:pt-9 sm:pb-7'>
+      <div className='mx-auto flex w-full max-w-80 items-baseline justify-between sm:max-w-5xl'>
         <p>TAKANORI HIDAKA</p>
         <p>Nav</p>
       </div>
