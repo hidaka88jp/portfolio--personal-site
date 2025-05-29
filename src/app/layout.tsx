5ff9ce670d3563d16320d2cdbb373d8dd1e0dd5b
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-05-29 13:47:24 -0700

    style: Set Inter as default font via font-sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Inter } from 'next/font/google';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
@@ -8,13 +9,18 @@ export const metadata: Metadata = {
   description: "Showcasing my projects and things I've learned",
 };
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className={inter.variable}>
       <head>
         <meta name='theme-color' content='#ffffff' />
       </head>
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className='px-4 pt-4 pb-5 sm:px-8 sm:pt-9 sm:pb-7'>
       <div className='mx-auto flex w-full max-w-80 items-baseline justify-between sm:max-w-5xl'>
-        <p>TAKANORI HIDAKA</p>
+        <p className='font-inter'>TAKANORI HIDAKA</p>
         <p>Nav</p>
       </div>
     </header>
