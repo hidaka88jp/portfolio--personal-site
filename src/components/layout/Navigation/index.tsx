c02edae93bd3ea121abe3b0a92e4ab3741415adc
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-06-03 10:12:03 -0700

    feat: Add SNS links and icons to sidebar

--- src/components/layout/Navigation/index.tsx
+++ src/components/layout/Navigation/index.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import { clsx } from 'clsx';
 import Image from 'next/image';
 import { MdArrowForward } from 'react-icons/md';
+import { FaLinkedin } from 'react-icons/fa6';
+import { FaSquareXTwitter } from 'react-icons/fa6';
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
@@ -38,26 +40,31 @@ export default function Navigation() {
 
       <nav
         className={clsx(
-          'bg-gray fixed inset-y-0 right-0 z-10 flex w-64 flex-col items-center justify-start text-white transition-transform duration-300 ease-in-out',
+          'bg-gray fixed inset-y-0 right-0 z-10 flex w-64 flex-col items-center justify-start text-lg text-white transition-transform duration-300 ease-in-out',
           isOpen ? 'translate-x-0' : 'translate-x-full'
         )}
       >
         <button
-          className='absolute top-4 right-4 h-11 w-11'
+          className='absolute top-4 right-1 h-11 w-11'
           onClick={() => setIsOpen(false)}
           aria-label='Close menu'
         >
           <MdArrowForward size={24} className='fill-white' />
         </button>
-        <a href='#notes' className='mb-4 pt-24 text-xl'>
-          About
-        </a>
-        <a href='#works' className='mb-4 text-xl'>
-          Works
-        </a>
-        <a href='#contact' className='text-xl'>
-          Notes
-        </a>
+        <div className='flex flex-col items-center space-y-4 pt-24'>
+          <a href='#about'>About</a>
+          <a href='#works'>Works</a>
+          <a href='#notes'>Notes</a>
+        </div>
+        <div className='mt-6 h-px w-8 bg-white' />
+        <div className='mt-6 flex items-center justify-center space-x-4 fill-white'>
+          <a href='https://ca.linkedin.com/in/takanori-hidaka-102568177'>
+            <FaLinkedin size={30} />
+          </a>
+          <a href='https://x.com/taka_hidaka_log'>
+            <FaSquareXTwitter size={30} />
+          </a>
+        </div>
       </nav>
     </>
   );
