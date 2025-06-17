2fe465ae2dae011f79ca0c41ea15536f46f681cb
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-06-17 10:28:18 -0700

    feat: Add Hero image

--- src/components/top/Hero/index.tsx
+++ src/components/top/Hero/index.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function Hero() {
-  return <p>Hero</p>;
+  return (
+    <section className='relative h-80 w-full sm:h-[32rem]'>
+      <Image
+        src='/images/hero.jpg'
+        alt='Hero Image'
+        fill
+        className='object-cover object-left'
+        priority
+      />
+    </section>
+  );
 }
