c28bc73499c058faa9a980b5eb1b9da04b223e10
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-18 09:23:30 -0700

    feat: Add text in Hero section

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -12,7 +12,7 @@ const antonio = Antonio({
 export default function Header() {
   return (
     <header className='px-4 pt-4 pb-5 sm:px-8 sm:pt-9 sm:pb-7'>
-      <div className='mx-auto flex w-full max-w-90 items-center justify-between sm:max-w-5xl sm:items-baseline'>
+      <div className='mx-auto flex w-full max-w-94 items-center justify-between sm:max-w-5xl sm:items-baseline'>
         <Link href={'/'}>
           <h1
             className={`${antonio.className} text-2xl tracking-[.18em] sm:text-4xl`}
--- src/components/top/Hero/index.tsx
+++ src/components/top/Hero/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className='relative h-80 w-full sm:h-[32rem]'>
+    <section className='relative h-80 w-full sm:h-96'>
       <Image
         src='/images/hero.jpg'
         alt='Hero Image'
@@ -10,6 +10,19 @@ export default function Hero() {
         className='object-cover object-left'
         priority
       />
+      <div className='absolute inset-0 flex items-center px-4 sm:px-8'>
+        <div className='mx-auto w-full max-w-90 text-white sm:max-w-5xl'>
+          <pre className='text-2xl'>
+            {`
+            while (code){
+              learn();
+              create();
+              drinkCoffee();
+            }
+            `}
+          </pre>
+        </div>
+      </div>
     </section>
   );
 }
