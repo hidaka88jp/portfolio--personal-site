3ddf2f61cb98a40f784f1c60a723428095625d69
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-21 10:21:30 -0700

    chore: Adjust About image layout with responsive aspect ratios

--- src/components/top/About/index.tsx
+++ src/components/top/About/index.tsx
@@ -1,10 +1,21 @@
 import TopSectionTitle from '@/components/shared/TopSectionTitle';
+import Image from 'next/image';
 
 export default function About() {
   return (
     <section id='about' className='px-4 pb-16 sm:px-8 sm:pb-20'>
       <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>
         <TopSectionTitle title='About' subTitle='Introduce Myself' />
+        <div className='grid gap-7 sm:grid-cols-2 sm:gap-10'>
+          <Image
+            src='/images/about.webp'
+            alt='About Image'
+            width={3024}
+            height={3780}
+            className='aspect-square w-full object-cover object-bottom sm:order-2 sm:aspect-auto'
+          />
+          <div className='h-[300px] w-full bg-amber-500 sm:order-1'></div>
+        </div>
       </div>
     </section>
   );
