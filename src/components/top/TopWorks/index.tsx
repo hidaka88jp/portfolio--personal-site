2b864a5a76b85889d9a4de0ab0c0ce92858c82f2
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-22 14:16:12 -0700

    feat: Add microcms.ts

--- src/components/top/TopWorks/index.tsx
+++ src/components/top/TopWorks/index.tsx
@@ -4,7 +4,7 @@ import TopSectionTitle from '@/components/shared/TopSectionTitle';
 
 export default function TopWorks() {
   return (
-    <section id='top-works' className='px-4 pb-16 sm:px-8 sm:pb-20'>
+    <section id='works' className='px-4 pb-16 sm:px-8 sm:pb-20'>
       <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>
         <TopSectionTitle title='Works' subTitle="What I've built" />
         <div className='grid grid-cols-1 gap-9 sm:grid-cols-2 xl:grid-cols-3 xl:gap-8'>
