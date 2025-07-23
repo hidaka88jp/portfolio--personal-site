35cbf2e3fcf868f81f03450004da27b550a9a1cb
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-23 11:02:59 -0700

    feat: Add title and grids to TopNotes

--- src/components/top/TopNotes/index.tsx
+++ src/components/top/TopNotes/index.tsx
@@ -1,8 +1,18 @@
+import TopSectionTitle from '@/components/shared/TopSectionTitle';
+
 export default function TopNotes() {
   return (
     <div>
-      <h1>Top Notes</h1>
-      {/* Additional content can be added here */}
+      <section id='works' className='px-4 pb-16 sm:px-8 sm:pb-20'>
+        <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>
+          <TopSectionTitle title='Note' subTitle="What I've learned" />
+          <div className='grid grid-cols-1 gap-9 sm:grid-cols-2 xl:grid-cols-3 xl:gap-8'>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
