7068a7447cd148e6006567d947d6ba8c2a2088bd
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-24 09:12:03 -0700

    feat: Add Back to Top Button

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -1,7 +1,19 @@
+'use client';
+import { IoIosArrowUp } from 'react-icons/io';
+
 export default function Footer() {
   return (
-    <footer className='bg-gray flex flex-col items-center justify-center py-8 sm:py-10'>
-      <p className='text-sm text-white'>© 2025 TAKANORI HIDAKA</p>
+    <footer>
+      <button
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        className='flex w-full cursor-pointer flex-col items-center justify-center bg-gray-300 py-1.5'
+      >
+        <IoIosArrowUp />
+        <p>Back to Top</p>
+      </button>
+      <div className='bg-gray flex flex-col items-center justify-center py-8 sm:py-10'>
+        <p className='text-sm text-white'>© 2025 TAKANORI HIDAKA</p>
+      </div>
     </footer>
   );
 }
