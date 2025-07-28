5e349ecc9781193f0d2c21cc9601b029a26839c5
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-28 10:17:31 -0700

    feat: Add Back to works button to works page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from 'next/navigation';
 import { getWorkDetail } from '@/lib/microcms';
+import LinkButton from '@/components/shared/LinkButton';
 
 type WorkDetailPageProps = {
   params: Promise<{ id: string }>; // Promise to ensure params are awaited before use
@@ -18,7 +19,13 @@ export default async function WorkDetailPage({ params }: WorkDetailPageProps) {
   return (
     <article className='px-4 pb-16 sm:px-8 sm:pb-28'>
       <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>
-        <div className='article' dangerouslySetInnerHTML={{ __html: work.content }} />
+        <div
+          className='article mb-10 md:mb-14'
+          dangerouslySetInnerHTML={{ __html: work.content }}
+        />
+        <div className='flex flex-col items-center justify-center'>
+          <LinkButton href={'/#works'}>Back to Works</LinkButton>
+        </div>
       </div>
     </article>
   );
