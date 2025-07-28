ba0c118e216789538ebf7a01b568a49d50ff4b76
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-28 13:12:55 -0700

    feat: Add /notes/page.tsx

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,7 @@
+export default function NotesPage() {
+  return (
+    <div className='px-4 pb-16 sm:px-8 sm:pb-28'>
+      <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>notes page</div>
+    </div>
+  );
+}
