940a3ee8c7f1684ba8d1332a8849c3627decc730
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-07-29 11:23:39 -0700

    feat: Add pagination and techStack filtering to notes API route

--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -3,10 +3,15 @@ import { getNotesList } from '@/lib/microcms';
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
+
   const techStack = searchParams.get('techStack') || '';
+  const page = Number(searchParams.get('page')) || 1;
+  const limit = 10; // one page limit
+  const offset = (page - 1) * limit;
 
   const filters = techStack ? `techStack[contains]${techStack}` : undefined;
-  const notes = await getNotesList({ filters });
+
+  const notes = await getNotesList({ filters, limit, offset });
 
   return NextResponse.json(notes);
 }
--- src/components/notes/NotesList/index.tsx
+++ src/components/notes/NotesList/index.tsx
@@ -10,15 +10,17 @@ export default function NotesList() {
   const isAll = selected === '';
 
   return (
-    <div
-      className={clsx(
-        'flex w-fit items-center gap-2 rounded-md border px-2 py-1',
-        isAll ? 'border-gray text-gray bg-transparent' : 'text-white'
-      )}
-      style={isAll ? {} : { backgroundColor: teckStack?.color, borderColor: teckStack?.color }}
-    >
-      {Icon && !isAll && <Icon className='h-6 w-6' />}
-      <p className='text-2xl'>{isAll ? 'ALL' : teckStack?.name}</p>
-    </div>
+    <section>
+      <div
+        className={clsx(
+          'flex w-fit items-center gap-2 rounded-md border px-2 py-1',
+          isAll ? 'border-gray text-gray bg-transparent' : 'text-white'
+        )}
+        style={isAll ? {} : { backgroundColor: teckStack?.color, borderColor: teckStack?.color }}
+      >
+        {Icon && !isAll && <Icon className='h-6 w-6' />}
+        <p className='text-2xl'>{isAll ? 'ALL' : teckStack?.name}</p>
+      </div>
+    </section>
   );
 }
