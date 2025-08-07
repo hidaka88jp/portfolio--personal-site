d2c067c2979635c312bf9605afe9b216a3ffa09e
hidaka88jp <hidaka88jp@yahoo.co.jp>
2025-08-07 10:49:33 -0700

    style: Ajust position for overlay and change image from png to webp in Hero

--- src/components/top/Hero/index.tsx
+++ src/components/top/Hero/index.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className='relative mb-20 h-80 w-full overflow-hidden sm:h-96'>
       <Image
-        src='/images/hero.jpg'
+        src='/images/hero.webp'
         alt='Hero Image'
         fill
         className='object-cover object-left'
@@ -12,7 +12,7 @@ export default function Hero() {
       />
       <div className='absolute inset-0 flex items-end px-4 sm:px-8'>
         <div className='relative mx-auto w-full max-w-90 px-5 py-7 sm:max-w-5xl sm:px-0 sm:py-14'>
-          <div className='absolute bottom-1 left-[-80px] h-48 w-[150%] origin-bottom-left rotate-[-8deg] bg-black opacity-60 sm:left-[-230px] sm:h-80 sm:origin-bottom-left sm:translate-y-[-400px] sm:rotate-[60deg]' />
+          <div className='absolute bottom-1 left-[-80px] h-48 w-[150%] origin-bottom-left translate-y-2 rotate-[-8deg] bg-black opacity-60 sm:left-[-230px] sm:h-80 sm:origin-bottom-left sm:translate-y-[-400px] sm:rotate-[60deg]' />
           <pre className='font-inconsolata relative z-10 text-2xl font-light text-white sm:text-3xl'>
             {`while (code) {
   learn();
